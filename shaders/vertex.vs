e802205ecf64f2f20b0405dfca563d2378b3fa65
TimothyHartt <tiha6812@colorado.edu>
2023-09-26 13:59:00 -0600

    Add git-ignore

--- shaders/vertex.vs
+++ shaders/vertex.vs
@@ -1,7 +1,6 @@
 #version 400 core
 layout (location = 0) in vec3 pos;
 
-// Removed Uniform
 void main() {
     gl_Position = vec4(pos.x, pos.y, pos.z, 1.0);
 }
--- src/main.rs
+++ src/main.rs
@@ -3,36 +3,6 @@ use merge::{*, helpf::shader_utils::*};
 use std::mem;
 //Pos datatye for shader input
 
-/*
-Opengl programs are not statically linked, they are linked at runtime, using drivers and features
-based on the user's system. There is uncertainty, your calls may fail, this is unsafe code in rust
-*/
-
-/*
-    Lets try and draw something more interesting. I want to put thing into a seperate glsl file, so lets figure that out.
-    I want to remake my grass project but with more complex shapes for the blades of grass and how they animate according to some external "wind".   
-    It's be great to have a way to load .obj or other 3d model file types. 
-
-
-    First, glsl files. 
-
-*/
-
-/* 
-Stage Two:
-    "World Objects"
-    Contains a World Matrix
-        - Includes Translation, Rotation, and Scale Mats
-    Possibly contains what texture(s) it needs that has been loaded on the gpu.
-    That should be it. It really shouldn't have a functon maybe besides some constuctor.
-
-    To do future:
-    World, View, Projection Matrix
-    For now we (probably don't need any of these for our decal test.)
-
-*/
-
-
 
 trait RenderGroup {
     fn gen_vo(&mut self);
@@ -110,7 +80,6 @@ fn set_vertex_attributes(){
     }
 }
 
-//Will take an inpute, with different function definitions for some situations 
 fn bind_buffer_data(){
 
 
@@ -154,22 +123,6 @@ fn main() {
     unsafe{
         sdl.gl_set_attribute(SdlGlAttr::DepthSize, 32).unwrap();
         load_gl_with(|fn_name| win.get_proc_address(fn_name));
-        /*
-
-            //We need to bind an array object and at least 1 buffer
-            //We need to setup the vertex attribute poiter, a template telling the GPU how the data being sent is organized.
-            //Need a vertex attributte pointer for each attribute, and need to enable it after each
-            We need the verticies that we will draw. I normally hate coming up with these manually, but whatevs 
-            We need to bind them to a buffer.
-            At some point we need to setup our graphics pipeline with a Vertex and Fragment shader.
-            Then clear the screan, then send our data to the gpu with the glDraw function
-            And finally we swap buffers to draw to the string, coolio 
-
-
-            First, lets create the shaders. 
-        */
-        //Creat Vertex Array & Buffer Objects
-        //These can really be in any order, just need to bind vertex obejects before binding or modify bound objects
 
         let mut shader = helpf::shader_utils::SimpleShader::default();
         shader.compile_shader().link_program().cach_uniforms().use_program();
@@ -203,17 +156,3 @@ fn main() {
 
 
 
-/*
-Of course functional programing has patterns. These exist to carry out common tasks while being safe, easy to do
-and re-usable. They get you thinking in a certain way. I think my time in OOAD make me really look down on the idea.
-It makes a lot of sense though. So, how about this. Lets start from zero and see if this increases the workload I can do.
-
-struct Color(i32, i32, i32);
-    A struct with no way to map a name to the values it holds. Can use self.0, self.1, ect.
-    So, when we say we use a struct to wrap a value what we're wrapping is the self.0, or whatever.
-    The fuctions and potential other values are just there to modify this wrapped value. 
-
-
-    
-
- */
